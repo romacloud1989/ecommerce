6f533036094f3abb84f97cecc5a15d7929669c30
romacloud <romacloud3@gmail.com>
2021-07-22 12:29:27 -0300

    Categorias VS Produtos

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -7,8 +7,6 @@ use \Hcode\Model;
 
 class Category extends Model {
 
-
-
   public static function listAll()
   {
 
@@ -68,13 +66,59 @@ class Category extends Model {
     $html = [];
 
     foreach ($categories as $row) {
+
       array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+
     }
 
     file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html" , implode('', $html));
 
   }
 
-}
+  public function getProducts($related = true)
+  {
 
+    $sql = new Sql();
+
+    if ($related === true) {
+
+      return $sql->select("SELECT * FROM tb_products WHERE idproduct IN(SELECT a.idproduct FROM tb_products a INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct WHERE b.idcategory = :idcategory);",[
+        'idcategory'=>$this->getidcategory()
+      ]);
+
+    } else {
+
+      return $sql->select("SELECT * FROM tb_products WHERE idproduct NOT IN(SELECT a.idproduct FROM tb_products a INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct WHERE b.idcategory = :idcategory);",[
+        'idcategory'=>$this->getidcategory()
+      ]);
+
+    }
+
+  }
+
+  public function addProduct(Product $product)
+  {
+
+    $sql = new Sql();
+
+    $sql->query("INSERT INTO tb_productscategories (idcategory, idproduct) VALUES(:idcategory, :idproduct)", [
+      ':idcategory'=>$this->getidcategory(),
+      'idproduct'=>$product->getidproduct()
+    ]);
+
+  }
+
+  public function removeProduct(Product $product)
+  {
+
+    $sql = new Sql();
+
+    $sql->query("DELETE FROM tb_productscategories WHERE idcategory = :idcategory AND idproduct = :idproduct", [
+      ':idcategory'=>$this->getidcategory(),
+      'idproduct'=>$product->getidproduct()
+    ]);
+
+  }
+
+}
  ?>
