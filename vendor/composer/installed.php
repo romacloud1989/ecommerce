55982e11e2ad5914c734661444ed2e663ec49e9f
Romario da Silva Cunha <romario.cunha@unicesumar.edu.br>
2021-08-17 20:30:34 -0300

    Categoryes details

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,51 +1,50 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5ecab78bb7cc773426b0a703721e123b5a8389ce',
+        'name' => 'romacloud1989/ecommerce',
+        'dev' => true,
     ),
-    'reference' => '0b5e3eb52646918f7ba14f679a9d8faf46ad7f51',
-    'name' => 'romacloud1989/ecommerce',
-  ),
-  'versions' => 
-  array (
-    'phpmailer/phpmailer' => 
-    array (
-      'pretty_version' => 'v5.2.22',
-      'version' => '5.2.22.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'b18cb98131bd83103ccb26a888fdfe3177b8a663',
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v5.2.22',
+            'version' => '5.2.22.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'b18cb98131bd83103ccb26a888fdfe3177b8a663',
+            'dev_requirement' => false,
+        ),
+        'rain/raintpl' => array(
+            'pretty_version' => '3.0.0',
+            'version' => '3.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rain/raintpl',
+            'aliases' => array(),
+            'reference' => '5bfdf4602b8adb0e80b222fa29ac559aa9c06abf',
+            'dev_requirement' => false,
+        ),
+        'romacloud1989/ecommerce' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5ecab78bb7cc773426b0a703721e123b5a8389ce',
+            'dev_requirement' => false,
+        ),
+        'slim/slim' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../slim/slim',
+            'aliases' => array(),
+            'reference' => '3e95c9abbc57a8f023ab4dacebcab9dae4d9f1f0',
+            'dev_requirement' => false,
+        ),
     ),
-    'rain/raintpl' => 
-    array (
-      'pretty_version' => '3.0.0',
-      'version' => '3.0.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '5bfdf4602b8adb0e80b222fa29ac559aa9c06abf',
-    ),
-    'romacloud1989/ecommerce' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '0b5e3eb52646918f7ba14f679a9d8faf46ad7f51',
-    ),
-    'slim/slim' => 
-    array (
-      'pretty_version' => '2.0.0',
-      'version' => '2.0.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '3e95c9abbc57a8f023ab4dacebcab9dae4d9f1f0',
-    ),
-  ),
 );
